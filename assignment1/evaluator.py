f0c71cd8024547fc0bd25daa6062799d56692528
bmyers60 <144624134+bmyers60@users.noreply.github.com>
2024-06-11 18:43:41 +0000

    Pending changes exported from your codespace

---
+++ assignment1/evaluator.py
@@ -0,0 +1,111 @@
+from tokenizer import tokenize
+from parser import parse
+
+def evaluate(ast, environment):
+    if ast["tag"] == "number":
+        assert type(ast["value"]) in [float, int], f"unexpected ast numeric value {ast['value']} is a {type(ast['value'])}."
+        return ast["value"], False
+    if ast["tag"] == "+":
+        left_value, _ = evaluate(ast["left"], environment)
+        right_value, _ = evaluate(ast["right"], environment)
+        return left_value + right_value, False
+    if ast["tag"] == "-":
+        left_value, _ = evaluate(ast["left"], environment)
+        right_value, _ = evaluate(ast["right"], environment)
+        return left_value - right_value, False
+    if ast["tag"] == "*":
+        left_value, _ = evaluate(ast["left"], environment)
+        right_value, _ = evaluate(ast["right"], environment)
+        return left_value * right_value, False
+    if ast["tag"] == "/":
+        left_value, _ = evaluate(ast["left"], environment)
+        right_value, _ = evaluate(ast["right"], environment)
+        return left_value / right_value, False
+    if ast["tag"] == "negate":
+        value, _ = evaluate(ast["value"], environment)
+        return -value, False
+    if ast["tag"] == "not":
+        value, _ = evaluate(ast["value"], environment)
+        if value == 0:
+            return 1, False
+        else:
+            return 0, False
+    raise Exception(f"Unknown token in AST: {ast['tag']}")
+
+def equals(code, environment, expected_result, expected_environment=None):
+    result, _ = evaluate(parse(tokenize(code)), environment)
+    assert (result == expected_result), f"""ERROR: When executing-- 
+        {[code]},
+        --expected--
+        {[expected_result]},
+        --got--
+        {[result]}."""
+    if expected_environment:
+        assert (
+            environment == expected_environment
+        ), f"""
+        ERROR: When executing 
+        {[code]}, 
+        expected
+        {[expected_environment]},\n got \n{[environment]}.
+        """
+
+
+def test_evaluate_single_value():
+    print("test evaluate single value")
+    equals("4", {}, 4, {})
+
+
+def test_evaluate_addition():
+    print("test evaluate addition")
+    equals("1+3", {}, 4)
+    equals("1+4", {}, 5)
+    equals("4+2", {}, 6)
+    equals("4+2+1", {}, 7)
+
+
+def test_evaluate_subtraction():
+    print("test evaluate subtraction")
+    equals("11-5", {}, 6)
+
+
+def test_evaluate_multiplication():
+    print("test evaluate multiplication")
+    equals("11*5", {}, 55)
+
+
+def test_evaluate_division():
+    print("test evaluate division")
+    equals("12/3", {}, 4)
+
+def test_evaluate_unary_negation():
+    print("test evaluate unary negation")
+    equals("-12/3", {}, -4)
+    equals("12/-3", {}, -4)
+    equals("12/--3", {}, 4)
+    equals("(3+4)--(1+2)", {}, 10)
+
+def test_not_expressions():
+    print("test not expressions")
+    equals("!2", {}, 0)
+    equals("!0", {}, 1)
+    equals("3*4*!2", {}, 0)
+    equals("!(3+4)*2", {}, 0)
+
+def test_evaluate_complex_expression():
+    print("test evaluate complex expression")
+    equals("(3+4)-(1+2)", {}, 4)
+    equals("3+4*2", {}, 11)
+    equals("(1+2)*3",{},9)
+
+
+if __name__ == "__main__":
+    test_evaluate_single_value()
+    test_evaluate_addition()
+    test_evaluate_subtraction()
+    test_evaluate_multiplication()
+    test_evaluate_division()
+    test_evaluate_unary_negation()
+    test_evaluate_complex_expression()
+    test_not_expressions()
+    print("done")
---
+++ working/tokenizer.py
@@ -0,0 +1,124 @@
+# tokenizer
+
+"""
+break character stream into tokens, provide a list of tokens
+
+    tokens = tokenize(string_of_code)
+"""
+
+import re
+
+patterns = [
+    ["\(", "("],
+    ["\)", ")"],
+    ["\*", "*"],
+    ["\/", "/"],
+    ["\+", "+"],
+    ["\-", "-"],
+    ["(\d*\.\d+)|(\d+\.\d*)|(\d+)", "number"],
+]
+
+for pattern in patterns:
+    pattern[0] = re.compile(pattern[0])
+
+
+def tokenize(characters):
+    tokens = []
+    position = 0
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match
+        # update position for next match
+        token = {
+            "tag": tag,
+            "value": match.group(0),
+            "position": position,
+        }
+        position = match.end()
+        tokens.append(token)
+    for token in tokens:
+        if token["tag"] == "number":
+            if "." in token["value"]:
+                token["value"] = float(token["value"])
+            else:
+                token["value"] = int(token["value"])
+    token = {
+        "tag": "end",
+        "value": "",
+        "position": position,
+    }
+    tokens.append(token)
+    return tokens
+
+
+def test_simple_tokens():
+    print("testing simple tokens")
+    assert tokenize("") == [{"tag": "end", "value": "", "position": 0}]
+    assert tokenize("*")[0]["tag"] == "*"
+    tokens = tokenize("*+")
+    assert tokens == [
+        {"tag": "*", "value": "*", "position": 0},
+        {"tag": "+", "value": "+", "position": 1},
+        {"tag": "end", "value": "", "position": 2},
+    ]
+    tokens = tokenize("*/+-()")
+    assert tokens == [
+        {"tag": "*", "value": "*", "position": 0},
+        {"tag": "/", "value": "/", "position": 1},
+        {"tag": "+", "value": "+", "position": 2},
+        {"tag": "-", "value": "-", "position": 3},
+        {"tag": "(", "value": "(", "position": 4},
+        {"tag": ")", "value": ")", "position": 5},
+        {"tag": "end", "value": "", "position": 6},
+    ]
+    tokens = tokenize("123")
+    assert tokens == [
+        {"tag": "number", "value": 123, "position": 0},
+        {"tag": "end", "value": "", "position": 3},
+    ]
+    tokens = tokenize("123.45")
+    assert tokens == [
+        {"tag": "number", "value": 123.45, "position": 0},
+        {"tag": "end", "value": "", "position": 6},
+    ]
+    tokens = tokenize("123.")
+    assert tokens == [
+        {"tag": "number", "value": 123.0, "position": 0},
+        {"tag": "end", "value": "", "position": 4},
+    ]
+    tokens = tokenize(".25")
+    assert tokens == [
+        {"tag": "number", "value": 0.25, "position": 0},
+        {"tag": "end", "value": "", "position": 3},
+    ]
+    
+
+
+def test_tokenize_expression():
+    print("testing tokenize expression")
+    tokens = tokenize("(3.5+40)/5-(3.*.4)")
+    assert tokens == [
+        {"tag": "(", "value": "(", "position": 0},
+        {"tag": "number", "value": 3.5, "position": 1},
+        {"tag": "+", "value": "+", "position": 4},
+        {"tag": "number", "value": 40, "position": 5},
+        {"tag": ")", "value": ")", "position": 7},
+        {"tag": "/", "value": "/", "position": 8},
+        {"tag": "number", "value": 5, "position": 9},
+        {"tag": "-", "value": "-", "position": 10},
+        {"tag": "(", "value": "(", "position": 11},
+        {"tag": "number", "value": 3.0, "position": 12},
+        {"tag": "*", "value": "*", "position": 14},
+        {"tag": "number", "value": 0.4, "position": 15},
+        {"tag": ")", "value": ")", "position": 17},
+        {"tag": "end", "value": "", "position": 18},
+    ]
+
+
+if __name__ == "__main__":
+    test_simple_tokens()
+    test_tokenize_expression()
+    print("done.")
